7650755689d18b65b0eea2293004e7b8371e873c
Philipp Beck <philipp.beck@anybill.de>
2023-02-07 21:37:39 +0100

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import '@heathmont/moon-themes-tw/lib/moon.css';
+import { Button } from '@heathmont/moon-core-tw';
+
+function App() {
+  return (
+    <div dir="ltr">
+      <Button>Test</Button>
+    </div>
+  )
+}
+
+export default App
